e9fe2daeefdfc57eaaa86a4fd837e8e4a7f126fc
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-02-22 08:57:04 +0600

    modification

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Users\Pages;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
-
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
