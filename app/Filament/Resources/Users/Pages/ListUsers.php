ad9b06d3d4242b36d5e63f53280b716f5bd69149
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-02-22 09:00:56 +0600

    modification

--- app/Filament/Resources/Users/Pages/ListUsers.php
+++ app/Filament/Resources/Users/Pages/ListUsers.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Users\Pages;
 use App\Filament\Resources\Users\UserResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
-
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
