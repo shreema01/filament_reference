d70f2ecd52b0d8454f0a1d543f6559d5eea0f3cd
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-02-22 08:56:40 +0600

    modification

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -4,7 +4,6 @@ namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
 use Filament\Resources\Pages\CreateRecord;
-
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
