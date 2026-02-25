1f447b66d746ea0d52ea7d02aa3c04fda770bb66
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-02-25 09:21:28 +0600

    modification

--- app/Filament/Resources/Users/UserResource.php
+++ app/Filament/Resources/Users/UserResource.php
@@ -17,11 +17,9 @@ use Filament\Tables\Table;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     protected static ?string $recordTitleAttribute = 'name';
-
     public static function form(Schema $schema): Schema
     {
        return UserForm::configure($schema);
